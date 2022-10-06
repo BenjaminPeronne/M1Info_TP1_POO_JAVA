115cf5dc419f2c8b59208318fc8e3f3b854270b3
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-06 15:16:26 -0400

    fist commit with remove `.DS_Store`

---
+++ src/main/java/tp_1/App.java
@@ -0,0 +1,18 @@
+/**
+ * @author Benjamin Peronne
+ * @email contact@benjaminperonne.fr
+ * @create date 2022-10-06 15:12:41
+ * @modify date 2022-10-06 15:12:41
+ * @desc [TP1 - POO]
+ */
+
+package tp_1;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
