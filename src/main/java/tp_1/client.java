f642058921e2ca02ffe515af89b4df1e6c77ea83
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-13 17:18:04 -0400

    Update `client.java`
    Update client.java with Attributs & client constructor

--- src/main/java/tp_1/client.java
+++ src/main/java/tp_1/client.java
@@ -7,7 +7,93 @@
  */
 
 package tp_1;
+
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class client {
     public static void main(String[] args) {
+        client client1 = new client("Client1", "192.168.1.9", 8003, "HTTP");
+        client1.afficher();
+        client1.envoyer();
+
+
+    // ==================== GETTERS & SETTERS ====================
+    public String getNom() {
+        return this.nom;
     }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresseIP() {
+        return this.adresseIP;
+    }
+
+    public void setAdresseIP(String adresseIP) {
+        this.adresseIP = adresseIP;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getProtocole() {
+        return this.protocole;
+    }
+
+    public void setProtocole(String protocole) {
+        this.protocole = protocole;
+    }
+
+    // ==================== Méthodes ====================
+    public void afficher() { // Affiche les informations du client
+        System.out.println("Nom : " + this.nom);
+        System.out.println("Adresse IP : " + this.adresseIP);
+        System.out.println("Port : " + this.port);
+        System.out.println("Protocole : " + this.protocole);
+    }
+
+    public void envoyer() { // Envoie un message au serveur
+        try {
+            Socket socket = new Socket(this.adresseIP, this.port);
+            OutputStream os = socket.getOutputStream();
+            os.write("Hello".getBytes());
+            os.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // public void envoyer() {
+    // Socket s = null;
+    // try {
+    // // Création d'un socket client
+    // s = new Socket(this.adresseIP, this.port);
+
+    // // Création d'un flux de sortie
+    // InputStream is = s.getInputStream();
+    // OutputStream os = s.getOutputStream();
+
+    // ObjectInputStream ois = new ObjectInputStream(is);
+    // ObjectOutputStream oos = new ObjectOutputStream(os);
+
+    // // Envoi d'un message
+    // oos.writeObject("Hello");
+
+    // // Fermeture des flux et du socket
+    // ois.close();
+    // oos.close();
+    // s.close();
+
+    // } catch (Exception e) {
+    // System.out.println(e);
+    // }
+    // }
 }
