7be7e6e54b07534d031a1ad5cfae2c44ff64c434
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-13 17:27:23 -0400

    update with removing `.DS_Store`

--- src/main/java/tp_1/App.java
+++ src/main/java/tp_1/App.java
@@ -8,11 +8,8 @@
 
 package tp_1;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
