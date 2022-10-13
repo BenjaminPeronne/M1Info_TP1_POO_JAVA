3667ce5bdd08b7c2c1ff2d8b1eba4043dec5be2a
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-13 17:16:45 -0400

    Create `client.java`

---
+++ src/main/java/tp_1/client.java
@@ -0,0 +1,13 @@
+/**
+ * @author Benjamin Peronne
+ * @email contact@benjaminperonne.fr
+ * @create date 2022-10-13 16:45:18
+ * @modify date 2022-10-13 16:45:18
+ * @desc [TP1 - Client]
+ */
+
+package tp_1;
+public class client {
+    public static void main(String[] args) {
+    }
+}
