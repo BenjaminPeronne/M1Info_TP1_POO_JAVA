ca6eabe81b1b9ee0df62c76a52743ae02c44e987
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-11-03 00:11:23 -0400

    Update `serveur.java`

--- src/main/java/tp_1/serveur.java
+++ src/main/java/tp_1/serveur.java
@@ -6,15 +6,11 @@
  * @desc [TP1 - Serveur]
  */
 
-/*
- * A faire :
-   * - Mettre en place une boucle while pour que le serveur reste en écoute et qui ne se coupe pas
-   * - Réaliser une méthode connexion() qui permet de se connecter au serveur afin de d'issocié l'envoie de message et la connexion
- */
-
 package tp_1;
 
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -28,6 +24,10 @@ public class serveur {
     }
 
     // ==================== Attributs ====================
+    protected int serverPort = 8003;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
     private String nom;
     private String adresseIP;
     private int port;
@@ -42,37 +42,16 @@ public class serveur {
     }
 
     // ==================== GETTERS & SETTERS ====================
-    public String getNom() {
-        return this.nom;
-    }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+    // public void setNom(String nom) {
+    // this.nom = nom;
+    // }
 
-    public String getAdresseIP() {
-        return this.adresseIP;
-    }
 
-    public void setAdresseIP(String adresseIP) {
-        this.adresseIP = adresseIP;
-    }
 
-    public int getPort() {
-        return this.port;
-    }
 
-    public void setPort(int port) {
-        this.port = port;
-    }
 
-    public String getProtocole() {
-        return this.protocole;
-    }
 
-    public void setProtocole(String protocole) {
-        this.protocole = protocole;
-    }
 
     // ==================== Méthodes ====================
     public void afficher() { // Affiche les informations du serveur
@@ -82,32 +61,88 @@ public class serveur {
         System.out.println("Protocole : " + this.protocole);
     }
 
-    public void ecouter() { // Méthode pour écouter les clients
-        try {
-            while (true) {
-                ServerSocket serverSocket = new ServerSocket(this.port);
-                System.out.println("Serveur en écoute sur le port " + this.port);
-
-                Socket socket = serverSocket.accept();
-                System.out.println("Connexion établie avec le client " + socket.getInetAddress().getHostAddress());
-
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
-
-                byte[] buffer = new byte[1024];
-                int nbOctets = inputStream.read(buffer);
-
-                String message = new String(buffer, 0, nbOctets);
-                System.out.println("Message reçu : " + message);
-
-                String messageReponse = "Bonjour, je suis le serveur " + this.nom;
-                outputStream.write(messageReponse.getBytes());
-
-                socket.close();
-                serverSocket.close();
+    public void ecouter() { // Ecoute le port du serveur
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        while (!isStopped()) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (Exception e) {
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException("Error accepting client connection", e);
             }
+            new Thread(new WorkerRunnable(clientSocket, "Multithreaded Server")).start();
+        }
+        System.out.println("Serveur arrêté.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erreur lors de la fermeture du servicer", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (Exception e) {
+            throw new RuntimeException("Impossible d'ouvrir le port 8003", e);
+        }
+    }
+
+    public class WorkerRunnable implements Runnable {
+
+        protected Socket clientSocket = null;
+        protected String serverText = null;
+
+        public WorkerRunnable(Socket clientSocket, String serverText) {
+            this.clientSocket = clientSocket;
+            this.serverText = serverText;
+        }
+
+        public void run() {
+            try {
+                InputStream input = clientSocket.getInputStream();
+                ObjectInputStream ois = new ObjectInputStream(input);
+
+                // Récupération de l'objet
+                // Object objet = ois.readObject();
+                // System.out.println("Objet reçu : " + objet);
+
+                String message = (String) ois.readObject();
+                System.out.println(
+                        "Message reçu : " + message + " de " + clientSocket.getInetAddress().getHostAddress());
+
+                OutputStream output = clientSocket.getOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(output);
+
+                // Envoi d'un message de confirmation au client
+                oos.writeObject("Envoi effectué avec succès");
+                oos.flush();
+
+                // Fermeture des flux
+                ois.close();
+                oos.close();
+
+                // Fermeture de la connexion
+                clientSocket.close();
+            } catch (Exception e) {
+                // report exception somewhere.
+                e.printStackTrace();
+            }
         }
     }
 }
